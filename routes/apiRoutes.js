736a93e63e4bd309aa9f0232465d37c03fdcbdde
your-SamGreenwood <your-sagg_84@hotmail.com>
2024-02-02 15:26:05 -0400

    testing note files in server, added unique id from uuidv4 library

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from 'url';
 import noteData from "../db/db.json" with { type: "json" };
 import fs from "fs";
+import { uuid } from 'uuidv4';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,11 +13,11 @@ const router = express.Router();
 router.get("/notes", (req, res) => {
     res.json(noteData);
 });
-
 router.post("/notes", (req, res) => {
+    req.body.id = uuid()
     noteData.push(req.body)
     console.log(noteData)
-
+    console.log(uuid())
 fs.writeFile('./db/db.json', JSON.stringify(noteData), function (err) {
   if (err) throw err;
   console.log('Saved!');
